cab151c7843017f9e6e940a8daa098298a039c95
fattoliu <724684054@qq.com>
2026-08-27 19:45:06 +0800

    test: align global mode with PAC-backed implementation

--- tests/test_system_proxy.py
+++ tests/test_system_proxy.py
@@ -2,7 +2,7 @@ from ssxng.config import AppConfig, ServerProfile
 from ssxng.core import SystemProxy
 
 
-def test_global_mode_clears_stale_http_proxies(monkeypatch, tmp_path):
+def test_global_mode_uses_local_global_pac(monkeypatch):
     cfg = AppConfig(profiles=[ServerProfile(server="example.com", password="x", local_port=1080)])
     calls = []
 
@@ -11,11 +11,10 @@ def test_global_mode_clears_stale_http_proxies(monkeypatch, tmp_path):
 
     SystemProxy(cfg).global_mode()
 
-    assert ("org.gnome.system.proxy.http", "host", "''") in calls
-    assert ("org.gnome.system.proxy.http", "port", "0") in calls
-    assert ("org.gnome.system.proxy.https", "host", "''") in calls
-    assert ("org.gnome.system.proxy.https", "port", "0") in calls
-    assert ("org.gnome.system.proxy.socks", "host", "'127.0.0.1'") in calls
-    assert ("org.gnome.system.proxy.socks", "port", "1080") in calls
-    assert calls[-1] == ("org.gnome.system.proxy", "mode", "'manual'")
+    assert (
+        "org.gnome.system.proxy",
+        "autoconfig-url",
+        f"'http://127.0.0.1:{cfg.pac_port}/global.pac'",
+    ) in calls
+    assert calls[-1] == ("org.gnome.system.proxy", "mode", "'auto'")
     assert cfg.mode == "global"
