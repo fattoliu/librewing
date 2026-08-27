8933d3bbf89e9b474e4e675aaa4752d7cad8b583
fattoliu <724684054@qq.com>
2026-08-27 20:16:57 +0800

    lint: clean GTK4 server manager imports

--- ssxng/server_manager4.py
+++ ssxng/server_manager4.py
@@ -8,7 +8,7 @@ import gi
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Adw, Gio, Gtk  # noqa: E402
+from gi.repository import Adw, Gtk  # noqa: E402
 
 from .config import AppConfig, ServerProfile
 from .i18n import tr
